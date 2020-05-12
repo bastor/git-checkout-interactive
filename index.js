0dc443191c7e889108ad60d734af2602c7ed45ab
Sebastian Maliszczak <sebastian.maliszczak@gmail.com>
2020-05-12 11:16:22 +0200

    Change to autocmplete

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
 #!/usr/bin/env node
 
-const { promisify } = require('util')
-const exec = promisify(require('child_process').exec)
-const prompts = require('prompts')
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
+const prompts = require("prompts");
+const matchSorter = require("match-sorter").default;
 
-async function run () {
-  const { stdout: branches } = await exec('git branch -v --sort=-committerdate')
+async function run() {
+  const { stdout: branches } = await exec(
+    "git branch -v --sort=-committerdate"
+  );
 
   const choices = branches
     .split(/\n/)
-    .filter(branch => !!branch.trim())
-    .map(branch => {
-      const [, flag, value, hint] = branch.match(/([* ]) +([^ ]+) +(.+)/)
-      return { value, hint, disabled: flag === '*' }
-    })
+    .filter((branch) => !!branch.trim())
+    .map((branch) => {
+      const [, flag, value, hint] = branch.match(/([* ]) +([^ ]+) +(.+)/);
+      return { value, hint, disabled: flag === "*" };
+    });
+  const suggestByValue = (input, choices) =>
+    Promise.resolve(
+      choices.filter((i) => {
+        const results = matchSorter([{ ...i }], input, { keys: ["value"] });
+        return results.length > 0;
+      })
+    );
 
   const { branch } = await prompts({
-    type: 'select',
-    name: 'branch',
-    message: 'Switch branch',
+    type: "autocomplete",
+    name: "branch",
+    suggest: suggestByValue,
+    message: "Switch branch",
     choices,
     hint: choices[0].hint,
-    warn: 'current branch',
-    onState ({ value }) {
-      this.hint = choices.find(c => c.value === value).hint
-    }
-  })
+    warn: "current branch",
+    onState({ value }) {
+      this.hint = choices.find((c) => c.value === value).hint;
+    },
+  });
 
-  await checkout(branch)
+  await checkout(branch);
 }
 
-async function checkout (branch) {
-  if (!branch) return
-  const { stdout, stderr } = await exec(`git checkout ${branch}`)
-  process.stdout.write(stdout)
-  process.stderr.write(stderr)
+async function checkout(branch) {
+  if (!branch) return;
+  const { stdout, stderr } = await exec(`git checkout ${branch}`);
+  process.stdout.write(stdout);
+  process.stderr.write(stderr);
 }
 
-function onError (e) {
+function onError(e) {
   if (e.stderr) {
-    process.stderr.write(e.stderr)
+    process.stderr.write(e.stderr);
   } else {
-    console.error(e)
+    console.error(e);
   }
 }
 
-run().catch(onError)
+run().catch(onError);
